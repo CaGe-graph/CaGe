39160a95003f604be49ca448b77419ddafa730a3
Adriaan Peeters <Adriaan.Peeters@UGent.be>
2008-08-19 13:04:04 +0000

    Added FormulaHCgenPanel to a tabbed pane

---
+++ cage/generator/HCgenPanel.java
@@ -0,0 +1,59 @@
+
+
+package cage.generator;
+
+
+import cage.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import lisken.systoolbox.*;
+import lisken.uitoolbox.*;
+
+
+public class HCgenPanel extends GeneratorPanel
+{
+
+    private GeneratorPanel lastChosenPanel = null;
+
+    private JTabbedPane pane = new JTabbedPane();
+
+    private FormulaHCgenPanel formulaHCgenPanel;
+
+    //private VulInPanel vulInPanel;
+
+    public HCgenPanel() {
+	formulaHCgenPanel = new FormulaHCgenPanel();
+	//vulInPanel = new VulInPanel();
+	pane.addTab("by formula", formulaHCgenPanel);
+	//pane.addTab("by degree list", vulInPanel);
+	for (int i = 0; i < pane.getTabCount(); i++) {
+	    ((GeneratorPanel) pane.getComponentAt(i)).setBorder(
+	        BorderFactory.createEmptyBorder(20, 20, 20, 20));
+	}
+	add(pane);
+
+	pane.addChangeListener(new ChangeListener() {
+		public void stateChanged(ChangeEvent event) {
+		    JTabbedPane pane = (JTabbedPane)event.getSource();
+		    ((GeneratorPanel)pane.getSelectedComponent()).showing();
+		}
+	    });
+    }
+
+    public void showing() {
+	((GeneratorPanel)pane.getSelectedComponent()).showing();
+	//formulaHCgenPanel.setDefaultButton(SwingUtilities.getRootPane(this).getDefaultButton());
+	//formulaHCgenPanel.showing();
+	//vulInPanel.showing();
+    }
+
+    public GeneratorInfo getGeneratorInfo() {
+	GeneratorPanel chosenPanel = (GeneratorPanel) pane.getSelectedComponent();
+	GeneratorInfo info = chosenPanel.getGeneratorInfo();
+	return info;
+    }
+
+}
