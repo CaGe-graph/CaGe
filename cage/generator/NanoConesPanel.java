3a374f12bc74526f4cfdc4ff93857d7c59d1be69
Adriaan Peeters <Adriaan.Peeters@UGent.be>
2008-11-23 18:17:56 +0000

    Added checkbox for symmetric and for mirror

--- cage/generator/NanoConesPanel.java
+++ cage/generator/NanoConesPanel.java
@@ -11,8 +11,8 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
-
 import javax.swing.JTextField;
+
 import lisken.systoolbox.Systoolbox;
 import lisken.uitoolbox.EnhancedSlider;
 
@@ -21,7 +21,9 @@ public class NanoConesPanel extends GeneratorPanel {
     EnhancedSlider numberOfPentagonsSlider = new EnhancedSlider();
     
     JTextField lengthOfSide = new JTextField();
-    JCheckBox symmetricBox = new JCheckBox();
+    JCheckBox symmetricBox;
+    JCheckBox iprBox;
+    JCheckBox mirrorBox;
 
     public NanoConesPanel() {
 	setLayout(new GridBagLayout());
@@ -36,6 +38,8 @@ public class NanoConesPanel extends GeneratorPanel {
     
 	JLabel numberOfPentagonsLabel = new JLabel("Number of pentagons");
         symmetricBox = new JCheckBox("symmetric / nearsymmetric");
+        iprBox = new JCheckBox("isolated pentagons (ipr)");
+        mirrorBox = new JCheckBox("Mirror-images are considered nonisomorphic");
         
         JLabel lengthOfSideLabel = new JLabel("Length of Side");
 
@@ -59,15 +63,26 @@ public class NanoConesPanel extends GeneratorPanel {
      new GridBagConstraints(0, 4, 1, 1, 1.0, 1.0,
      GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
      new Insets(0, 10, 0, 10), 0, 0));
+    add(iprBox,
+     new GridBagConstraints(0, 5, 1, 1, 1.0, 1.0,
+     GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+     new Insets(0, 10, 0, 10), 0, 0));
+    add(mirrorBox,
+     new GridBagConstraints(0, 6, 1, 1, 1.0, 1.0,
+     GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+     new Insets(0, 10, 0, 10), 0, 0));
     }
 
     public GeneratorInfo getGeneratorInfo() {
 	String symmetric = symmetricBox.isSelected() ? " s" : "n";
 	String pentagons = "" + numberOfPentagonsSlider.getValue();
         String length = "" + lengthOfSide.getText();
+        
+        String ipr = iprBox.isSelected() ? " -i" : "";
+        String mirror = mirrorBox.isSelected() ? " -m" : "";
 
 	return new StaticGeneratorInfo(
-            Systoolbox.parseCmdLine("cone " +  pentagons + " " + length + " " + symmetric),
+            Systoolbox.parseCmdLine("cone " +  pentagons + " " + length + " " + symmetric + ipr + mirror),
 	    EmbedFactory.createEmbedder(new String[][]{{"embed"}}, new String[][]{{"embed", "-d3"}}),
 	    "test",
 	    6);
