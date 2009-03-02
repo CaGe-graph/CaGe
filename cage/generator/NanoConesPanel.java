a2763dd5641899681bd7e64fee5c7fb83a8b5aab
nvcleemp <nico.vancleemput@gmail.com>
2009-03-02 13:13:43 +0000

    reformatted code

--- cage/generator/NanoConesPanel.java
+++ cage/generator/NanoConesPanel.java
@@ -1,4 +1,3 @@
-
 package cage.generator;
 
 import cage.EmbedFactory;
@@ -21,9 +20,8 @@ import lisken.systoolbox.Systoolbox;
 import lisken.uitoolbox.EnhancedSlider;
 
 public class NanoConesPanel extends GeneratorPanel implements ActionListener {
-    
+
     private EnhancedSlider numberOfPentagonsSlider = new EnhancedSlider();
-    
     private JTextField lengthOfSide = new JTextField();
     private JRadioButton symmetricButton;
     private JRadioButton nearSymmetricButton;
@@ -32,8 +30,8 @@ public class NanoConesPanel extends GeneratorPanel implements ActionListener {
     private JCheckBox mirrorBox;
 
     public NanoConesPanel() {
-	setLayout(new GridBagLayout());
-        
+        setLayout(new GridBagLayout());
+
         numberOfPentagonsSlider.setMinimum(1);
         numberOfPentagonsSlider.setMaximum(5);
         numberOfPentagonsSlider.setMinorTickSpacing(1);
@@ -41,70 +39,70 @@ public class NanoConesPanel extends GeneratorPanel implements ActionListener {
         numberOfPentagonsSlider.setPaintTicks(true);
         numberOfPentagonsSlider.setPaintLabels(true);
         numberOfPentagonsSlider.setSnapToTicks(true);
-    
-	JLabel numberOfPentagonsLabel = new JLabel("Number of pentagons");
-        
+
+        JLabel numberOfPentagonsLabel = new JLabel("Number of pentagons");
+
         symmetricButton = new JRadioButton("symmetric");
         nearSymmetricButton = new JRadioButton("nearsymmetric");
-	symmetricButton.addActionListener(this);
+        symmetricButton.addActionListener(this);
         nearSymmetricButton.addActionListener(this);
-	symmetricGroup = new ButtonGroup();
-	symmetricGroup.add(symmetricButton);
-	symmetricGroup.add(nearSymmetricButton);
-	symmetricButton.setSelected(true);
-        
+        symmetricGroup = new ButtonGroup();
+        symmetricGroup.add(symmetricButton);
+        symmetricGroup.add(nearSymmetricButton);
+        symmetricButton.setSelected(true);
+
         iprBox = new JCheckBox("isolated pentagons (ipr)");
         mirrorBox = new JCheckBox("Mirror-images are considered nonisomorphic");
-        
+
         JLabel lengthOfSideLabel = new JLabel("Length of Side");
 
-    add(numberOfPentagonsLabel,
-     new GridBagConstraints(0, 0, 2, 1, 1.0, 1.0,
-     GridBagConstraints.WEST, GridBagConstraints.NONE,
-     new Insets(0, 10, 5, 10), 0, 0));
-    add(numberOfPentagonsSlider,
-     new GridBagConstraints(0, 1, 2, 1, 1.0, 1.0,
-     GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
-     new Insets(0, 10, 0, 10), 0, 0));
-    add(lengthOfSideLabel,
-     new GridBagConstraints(0, 2, 2, 1, 1.0, 1.0,
-     GridBagConstraints.WEST, GridBagConstraints.NONE,
-     new Insets(0, 10, 5, 10), 0, 0));
-    add(lengthOfSide,
-     new GridBagConstraints(0, 3, 2, 1, 1.0, 1.0,
-     GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
-     new Insets(0, 10, 0, 10), 0, 0));
-    add(symmetricButton,
-     new GridBagConstraints(0, 4, 1, 1, 1.0, 1.0,
-     GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
-     new Insets(0, 10, 0, 10), 0, 0));
-    add(nearSymmetricButton,
-     new GridBagConstraints(1, 4, 1, 1, 1.0, 1.0,
-     GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
-     new Insets(0, 10, 0, 10), 0, 0));
-    add(iprBox,
-     new GridBagConstraints(0, 5, 2, 1, 1.0, 1.0,
-     GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
-     new Insets(0, 10, 0, 10), 0, 0));
-    add(mirrorBox,
-     new GridBagConstraints(0, 6, 2, 1, 1.0, 1.0,
-     GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
-     new Insets(0, 10, 0, 10), 0, 0));
+        add(numberOfPentagonsLabel,
+                new GridBagConstraints(0, 0, 2, 1, 1.0, 1.0,
+                GridBagConstraints.WEST, GridBagConstraints.NONE,
+                new Insets(0, 10, 5, 10), 0, 0));
+        add(numberOfPentagonsSlider,
+                new GridBagConstraints(0, 1, 2, 1, 1.0, 1.0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+                new Insets(0, 10, 0, 10), 0, 0));
+        add(lengthOfSideLabel,
+                new GridBagConstraints(0, 2, 2, 1, 1.0, 1.0,
+                GridBagConstraints.WEST, GridBagConstraints.NONE,
+                new Insets(0, 10, 5, 10), 0, 0));
+        add(lengthOfSide,
+                new GridBagConstraints(0, 3, 2, 1, 1.0, 1.0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+                new Insets(0, 10, 0, 10), 0, 0));
+        add(symmetricButton,
+                new GridBagConstraints(0, 4, 1, 1, 1.0, 1.0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+                new Insets(0, 10, 0, 10), 0, 0));
+        add(nearSymmetricButton,
+                new GridBagConstraints(1, 4, 1, 1, 1.0, 1.0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+                new Insets(0, 10, 0, 10), 0, 0));
+        add(iprBox,
+                new GridBagConstraints(0, 5, 2, 1, 1.0, 1.0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+                new Insets(0, 10, 0, 10), 0, 0));
+        add(mirrorBox,
+                new GridBagConstraints(0, 6, 2, 1, 1.0, 1.0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+                new Insets(0, 10, 0, 10), 0, 0));
     }
 
     public GeneratorInfo getGeneratorInfo() {
-	String symmetric = symmetricButton.isSelected() ? " s" : "n";
-	String pentagons = "" + numberOfPentagonsSlider.getValue();
+        String symmetric = symmetricButton.isSelected() ? " s" : "n";
+        String pentagons = "" + numberOfPentagonsSlider.getValue();
         String length = "" + lengthOfSide.getText();
-        
+
         String ipr = iprBox.isSelected() ? " -i" : "";
         String mirror = mirrorBox.isSelected() ? " -m" : "";
 
-	return new StaticGeneratorInfo(
-            Systoolbox.parseCmdLine("cone " +  pentagons + " " + length + " " + symmetric + ipr + mirror),
-	    EmbedFactory.createEmbedder(new String[][]{{"embed"}}, new String[][]{{"embed", "-d3"}}),
-	    "test",
-	    6);
+        return new StaticGeneratorInfo(
+                Systoolbox.parseCmdLine("cone " + pentagons + " " + length + " " + symmetric + ipr + mirror),
+                EmbedFactory.createEmbedder(new String[][]{{"embed"}}, new String[][]{{"embed", "-d3"}}),
+                "test",
+                6);
     }
 
     public void showing() {
