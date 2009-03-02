43e058318ff310cee9c6e94b3971dde5f4fbdebe
nvcleemp <nico.vancleemput@gmail.com>
2009-03-02 14:06:59 +0000

    Rearranged nanocones panel

--- cage/generator/NanoConesPanel.java
+++ cage/generator/NanoConesPanel.java
@@ -14,20 +14,22 @@ import javax.swing.ButtonGroup;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
-import javax.swing.JTextField;
 
 import lisken.systoolbox.Systoolbox;
 import lisken.uitoolbox.EnhancedSlider;
+import lisken.uitoolbox.SpinButton;
 
 public class NanoConesPanel extends GeneratorPanel implements ActionListener {
 
     private EnhancedSlider numberOfPentagonsSlider = new EnhancedSlider();
-    private JTextField lengthOfSide = new JTextField();
+    private SpinButton lengthOfSide = new SpinButton( 0, 0, Integer.MAX_VALUE);
+    private SpinButton hexagonLayers = new SpinButton(1, 1, Integer.MAX_VALUE);
     private JRadioButton symmetricButton;
     private JRadioButton nearSymmetricButton;
     private ButtonGroup symmetricGroup;
     private JCheckBox iprBox;
     private JCheckBox mirrorBox;
+    private JCheckBox hexagonLayersBox;
 
     public NanoConesPanel() {
         setLayout(new GridBagLayout());
@@ -52,51 +54,64 @@ public class NanoConesPanel extends GeneratorPanel implements ActionListener {
         symmetricButton.setSelected(true);
 
         iprBox = new JCheckBox("isolated pentagons (ipr)");
-        mirrorBox = new JCheckBox("Mirror-images are considered nonisomorphic");
+        mirrorBox = new JCheckBox("Mirror-images to be considered nonisomorphic");
+        hexagonLayersBox = new JCheckBox("Add a number of hexagon layers");
+        hexagonLayers.setEnabled(hexagonLayersBox.isSelected());
+        hexagonLayersBox.addActionListener(new HexagonLayersBoxListener());
 
-        JLabel lengthOfSideLabel = new JLabel("Length of Side");
+        JLabel lengthOfSideLabel = new JLabel("Length of longest Side");
 
-        add(numberOfPentagonsLabel,
-                new GridBagConstraints(0, 0, 2, 1, 1.0, 1.0,
-                GridBagConstraints.WEST, GridBagConstraints.NONE,
-                new Insets(0, 10, 5, 10), 0, 0));
-        add(numberOfPentagonsSlider,
-                new GridBagConstraints(0, 1, 2, 1, 1.0, 1.0,
-                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
-                new Insets(0, 10, 0, 10), 0, 0));
-        add(lengthOfSideLabel,
-                new GridBagConstraints(0, 2, 2, 1, 1.0, 1.0,
-                GridBagConstraints.WEST, GridBagConstraints.NONE,
-                new Insets(0, 10, 5, 10), 0, 0));
-        add(lengthOfSide,
-                new GridBagConstraints(0, 3, 2, 1, 1.0, 1.0,
-                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
-                new Insets(0, 10, 0, 10), 0, 0));
         add(symmetricButton,
-                new GridBagConstraints(0, 4, 1, 1, 1.0, 1.0,
+                new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0,
                 GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
                 new Insets(0, 10, 0, 10), 0, 0));
         add(nearSymmetricButton,
-                new GridBagConstraints(1, 4, 1, 1, 1.0, 1.0,
+                new GridBagConstraints(1, 0, 1, 1, 1.0, 1.0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+                new Insets(0, 10, 0, 10), 0, 0));
+        add(numberOfPentagonsLabel,
+                new GridBagConstraints(0, 1, 1, 1, 1.0, 1.0,
+                GridBagConstraints.WEST, GridBagConstraints.NONE,
+                new Insets(0, 10, 5, 10), 0, 0));
+        add(numberOfPentagonsSlider,
+                new GridBagConstraints(1, 1, 1, 1, 1.0, 1.0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+                new Insets(0, 10, 0, 10), 0, 0));
+        add(lengthOfSideLabel,
+                new GridBagConstraints(0, 2, 1, 1, 1.0, 1.0,
+                GridBagConstraints.WEST, GridBagConstraints.NONE,
+                new Insets(0, 10, 5, 10), 0, 0));
+        add(lengthOfSide,
+                new GridBagConstraints(1, 2, 1, 1, 1.0, 1.0,
                 GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
                 new Insets(0, 10, 0, 10), 0, 0));
         add(iprBox,
-                new GridBagConstraints(0, 5, 2, 1, 1.0, 1.0,
+                new GridBagConstraints(0, 3, 2, 1, 1.0, 1.0,
                 GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
                 new Insets(0, 10, 0, 10), 0, 0));
         add(mirrorBox,
-                new GridBagConstraints(0, 6, 2, 1, 1.0, 1.0,
+                new GridBagConstraints(0, 4, 2, 1, 1.0, 1.0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+                new Insets(0, 10, 0, 10), 0, 0));
+        add(hexagonLayersBox,
+                new GridBagConstraints(0, 5, 1, 1, 1.0, 1.0,
+                GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+                new Insets(0, 10, 0, 10), 0, 0));
+        add(hexagonLayers,
+                new GridBagConstraints(1, 5, 1, 1, 1.0, 1.0,
                 GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
                 new Insets(0, 10, 0, 10), 0, 0));
     }
 
     public GeneratorInfo getGeneratorInfo() {
         String symmetric = symmetricButton.isSelected() ? " s" : "n";
-        String pentagons = "" + numberOfPentagonsSlider.getValue();
-        String length = "" + lengthOfSide.getText();
+        String pentagons = Integer.toString(numberOfPentagonsSlider.getValue());
+        String length = Integer.toString(lengthOfSide.getValue() - (symmetricButton.isSelected() ? 0 : 1));
 
         String ipr = iprBox.isSelected() ? " -i" : "";
         String mirror = mirrorBox.isSelected() ? " -m" : "";
+
+        String layers = hexagonLayersBox.isSelected() ? Integer.toString(hexagonLayers.getValue()) : "" ;
 
         return new StaticGeneratorInfo(
                 Systoolbox.parseCmdLine("cone " + pentagons + " " + length + " " + symmetric + ipr + mirror),
@@ -121,5 +136,13 @@ public class NanoConesPanel extends GeneratorPanel implements ActionListener {
             numberOfPentagonsSlider.setLabelTable(null);
             numberOfPentagonsSlider.setMajorTickSpacing(2);
         }
+    }
+
+    private class HexagonLayersBoxListener implements ActionListener{
+
+        public void actionPerformed(ActionEvent e) {
+            hexagonLayers.setEnabled(hexagonLayersBox.isSelected());
+        }
+
     }
 }
