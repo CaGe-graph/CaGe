7331a137f0f4d067d290e461dc813695af4c0265
nvcleemp <nico.vancleemput@gmail.com>
2009-03-11 16:45:59 +0000

    ExternalPanel: Added input from file as a separate external generator

--- cage/generator/ExternalPanel.java
+++ cage/generator/ExternalPanel.java
@@ -6,6 +6,10 @@ import cage.GeneratorInfo;
 import cage.GeneratorPanel;
 import cage.StaticGeneratorInfo;
 import cage.ValencyElementRule;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -23,26 +27,39 @@ import javax.swing.JSeparator;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+
 import lisken.systoolbox.Systoolbox;
 import lisken.uitoolbox.EnhancedSlider;
 import lisken.uitoolbox.JTextComponentFocusSelector;
 
+/**
+ * Panel that allows the configuration of a generator that is not in
+ * CaGe by default.
+ */
 public class ExternalPanel extends GeneratorPanel implements ActionListener {
 
-    JButton defaultButton;
-    JTextField externalCmd;
-    ButtonGroup embeddedModeGroup = new ButtonGroup();
-    ButtonGroup embedTemplateGroup = new ButtonGroup();
-    JLabel embedTemplateLabel = new JLabel();
-    boolean nonNullCmd, embedderRequired;
-    boolean embedderIsConstant = false;
-    EnhancedSlider embedIntensity = new EnhancedSlider();
-    JCheckBox embedExpertBox = new JCheckBox();
+    private JButton defaultButton;
+    private JRadioButton externalCmdButton = new JRadioButton("generator command: ");
+    private JTextField externalCmd;
+    private JRadioButton fromFileButton = new JRadioButton("Input from file: ");
+    private JTextField fromFileCmd;
+    private ButtonGroup commandGroup = new ButtonGroup();
+    private ButtonGroup embeddedModeGroup = new ButtonGroup();
+    private ButtonGroup embedTemplateGroup = new ButtonGroup();
+    private JLabel embedTemplateLabel = new JLabel();
+    private boolean nonNullCmd, embedderRequired;
+    private boolean embedderIsConstant = false;
+    private EnhancedSlider embedIntensity = new EnhancedSlider();
+    private JCheckBox embedExpertBox = new JCheckBox();
 
     public ExternalPanel() {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        //------------Command section-----------------
         JPanel commandPanel = new JPanel();
         externalCmd = new JTextField(30);
         nonNullCmd = false;
@@ -63,12 +80,70 @@ public class ExternalPanel extends GeneratorPanel implements ActionListener {
             }
         });
         new JTextComponentFocusSelector(externalCmd);
-        JLabel externalCmdLabel = new JLabel("generator command:  ");
-        externalCmdLabel.setLabelFor(externalCmd);
-        externalCmdLabel.setDisplayedMnemonic(KeyEvent.VK_C);
-        commandPanel.add(externalCmdLabel);
-        commandPanel.add(externalCmd);
+        commandGroup.add(externalCmdButton);
+        externalCmdButton.setMnemonic(KeyEvent.VK_C);
+        externalCmdButton.setSelected(true);
+        externalCmdButton.addChangeListener(new ChangeListener() {
+
+            public void stateChanged(ChangeEvent e) {
+                externalCmd.setEnabled(externalCmdButton.isSelected());
+                if(externalCmdButton.isSelected()){
+                    externalCmd.requestFocusInWindow();
+                    checkCmd();
+                }
+            }
+        });
+
+        fromFileCmd = new JTextField(30);
+        fromFileCmd.setActionCommand("f");
+        fromFileCmd.addActionListener(this);
+        fromFileCmd.getDocument().addDocumentListener(new DocumentListener() {
+
+            public void insertUpdate(DocumentEvent e) {
+                checkCmd();
+            }
+
+            public void removeUpdate(DocumentEvent e) {
+                checkCmd();
+            }
+
+            public void changedUpdate(DocumentEvent e) {
+                checkCmd();
+            }
+        });
+        fromFileCmd.setEnabled(false);
+        new JTextComponentFocusSelector(fromFileCmd);
+        commandGroup.add(fromFileButton);
+        fromFileButton.setMnemonic(KeyEvent.VK_F);
+        fromFileButton.setSelected(false);
+        fromFileButton.addChangeListener(new ChangeListener() {
+
+            public void stateChanged(ChangeEvent e) {
+                fromFileCmd.setEnabled(fromFileButton.isSelected());
+                if(fromFileButton.isSelected()){
+                    fromFileCmd.requestFocusInWindow();
+                    checkCmd();
+                }
+            }
+        });
+
+        //commandPanel.add(externalCmdLabel);
+        commandPanel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints(0, 0, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0,0,0,0), 5, 5);
+        commandPanel.add(externalCmdButton, gbc);
+        gbc.gridx++;
+        commandPanel.add(externalCmd, gbc);
+        gbc.gridx = 0;
+        gbc.gridy++;
+        commandPanel.add(fromFileButton, gbc);
+        gbc.gridx++;
+        commandPanel.add(fromFileCmd, gbc);
         add(commandPanel);
+        add(Box.createVerticalStrut(20));
+        add(new JSeparator(SwingConstants.HORIZONTAL));
+        add(Box.createVerticalStrut(20));
+
+        //------------Embedded Mode section-----------------
         JPanel embeddedModePanel = new JPanel();
         embeddedModePanel.setLayout(new BoxLayout(embeddedModePanel, BoxLayout.Y_AXIS));
         JRadioButton embeddedModeIgnore = new JRadioButton();
@@ -93,13 +168,12 @@ public class ExternalPanel extends GeneratorPanel implements ActionListener {
         embeddedModeGroup.add(embeddedModeRefine);
         embeddedModePanel.add(embeddedModeRefine);
         embeddedModeIgnore.doClick();
-        add(Box.createVerticalStrut(20));
-        add(new JSeparator(SwingConstants.HORIZONTAL));
-        add(Box.createVerticalStrut(20));
         add(embeddedModePanel);
         add(Box.createVerticalStrut(20));
         add(new JSeparator(SwingConstants.HORIZONTAL));
         add(Box.createVerticalStrut(20));
+
+        //------------Embedded Template section-----------------
         add(embedTemplateLabel);
         add(Box.createVerticalStrut(5));
         JPanel embedTemplatePanel = new JPanel();
@@ -137,6 +211,8 @@ public class ExternalPanel extends GeneratorPanel implements ActionListener {
         JLabel embedIntensityLabel = new JLabel("embed iteration count (% of default value)");
         embedIntensityLabel.setLabelFor(embedIntensity.slider());
         embedIntensityLabel.setDisplayedMnemonic(KeyEvent.VK_I);
+
+        //------------Embedded intensity section-----------------
         add(Box.createVerticalStrut(20));
         add(embedIntensityLabel);
         add(Box.createVerticalStrut(5));
@@ -151,13 +227,13 @@ public class ExternalPanel extends GeneratorPanel implements ActionListener {
         add(embedExpertBox);
     }
 
-    void checkCmd() {
-        String content = externalCmd.getText();
+    private void checkCmd() {
+        String content = externalCmdButton.isSelected() ? externalCmd.getText() : fromFileCmd.getText();
         nonNullCmd = !content.replace('|', ' ').trim().equals("");
         checkNextEnabled();
     }
 
-    void checkNextEnabled() {
+    private void checkNextEnabled() {
         if (defaultButton != null) {
             defaultButton.setEnabled(nonNullCmd && !(embedderRequired && embedTemplateGroup.getSelection() == null));
         }
@@ -167,6 +243,7 @@ public class ExternalPanel extends GeneratorPanel implements ActionListener {
         char actionCommand = e.getActionCommand().charAt(0);
         switch (actionCommand) {
             case 'c':
+            case 'f':
                 if (defaultButton != null) {
                     defaultButton.doClick();
                 }
@@ -193,7 +270,12 @@ public class ExternalPanel extends GeneratorPanel implements ActionListener {
         }
     }
 
-    void enableEmbedControls(boolean enabled) {
+    /**
+     * Sets whether or not the embed controls are enabled.
+     *
+     * @param enabled true if this component should be enabled, false otherwise
+     */
+    public void enableEmbedControls(boolean enabled) {
         Enumeration e = embedTemplateGroup.getElements();
         while (e.hasMoreElements()) {
             ((AbstractButton) e.nextElement()).setEnabled(enabled);
@@ -228,7 +310,12 @@ public class ExternalPanel extends GeneratorPanel implements ActionListener {
         embedder.setMode(embeddedMode);
         boolean embedExpert =
                 embedExpertBox.isEnabled() && embedExpertBox.isSelected();
-        String generatorCmdLine = externalCmd.getText();
+        String generatorCmdLine;
+        if(externalCmdButton.isSelected()){
+            generatorCmdLine = externalCmd.getText();
+        } else {
+            generatorCmdLine = "cat " + fromFileCmd.getText();
+        }
         StringBuffer filename = new StringBuffer();
         boolean inWord = false;
         int n = generatorCmdLine.length();
@@ -253,9 +340,9 @@ public class ExternalPanel extends GeneratorPanel implements ActionListener {
                 0,
                 templateInfo != null ? templateInfo.getElementRule() : new ValencyElementRule("H O C Si N S I"),
                 GeneratorInfo.createExpertMode(
-                embedExpert ? GeneratorInfo.EMBED_EXPERT : 0,
                 GeneratorInfo.GENERATOR_EXPERT |
-                (embedExpert ? 0 : GeneratorInfo.EMBED_EXPERT)));
+                (embedExpert ? GeneratorInfo.EMBED_EXPERT : 0),
+                embedExpert ? 0 : GeneratorInfo.EMBED_EXPERT));
     }
 
     public void showing() {
