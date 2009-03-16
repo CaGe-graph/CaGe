ac23057e85527e2d5fefc76b7bcadc1273613ba7
nvcleemp <nico.vancleemput@gmail.com>
2009-03-16 08:23:33 +0000

    GeneratorPanel implements WizardAwareComponent

--- cage/GeneratorPanel.java
+++ cage/GeneratorPanel.java
@@ -1,8 +1,113 @@
 package cage;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
-public abstract class GeneratorPanel extends JPanel {
+import lisken.uitoolbox.WizardAwareComponent;
+
+public abstract class GeneratorPanel extends JPanel implements WizardAwareComponent {
+
+    private JButton nextButton;
+
+    /**
+     * Get the current <i>next</i> button in the wizard.
+     *
+     * @return the value of nextButton
+     */
+    public JButton getNextButton() {
+        return nextButton;
+    }
+
+    /**
+     * Set the current <i>next</i> button in the wizard. This button can then
+     * be disabled by this panel in case of a non-valid state.
+     *
+     * @param nextButton The next button of the current stage
+     */
+    public void setNextButton(JButton nextButton) {
+        this.nextButton = nextButton;
+    }
+
+    private JButton previousButton;
+
+    /**
+     * Get the current <i>previous</i> button in the wizard.
+     *
+     * @return the value of previousButton
+     */
+    public JButton getPreviousButton() {
+        return previousButton;
+    }
+
+    /**
+     * Set the current <i>previous</i> button in the wizard.
+     *
+     * @param previousButton The previous button of the current stage
+     */
+    public void setPreviousButton(JButton previousButton) {
+        this.previousButton = previousButton;
+    }
+
+    private JButton finishButton;
+
+    /**
+     * Get the current <i>finish</i> button in the wizard.
+     *
+     * @return the value of finishButton
+     */
+    public JButton getFinishButton() {
+        return finishButton;
+    }
+
+    /**
+     * Set the current <i>finish</i> button in the wizard. This button can then
+     * be disabled by this panel in case of a non-valid state.
+     *
+     * @param finishButton The finish button of the current stage
+     */
+    public void setFinishButton(JButton finishButton) {
+        this.finishButton = finishButton;
+    }
+
+    private JButton cancelButton;
+
+    /**
+     * Get the current <i>cancel</i> button in the wizard.
+     *
+     * @return the value of cancelButton
+     */
+    public JButton getCancelButton() {
+        return cancelButton;
+    }
+
+    /**
+     * Set the current <i>cancel</i> button in the wizard.
+     *
+     * @param cancelButton The cancel button of the current stage
+     */
+    public void setCancelButton(JButton cancelButton) {
+        this.cancelButton = cancelButton;
+    }
+
+    private JButton exitButton;
+
+    /**
+     * Get the current <i>exit</i> button in the wizard.
+     *
+     * @return the value of exitButton
+     */
+    public JButton getExitButton() {
+        return exitButton;
+    }
+
+    /**
+     * Set the current <i>exit</i> button in the wizard.
+     *
+     * @param exitButton The exit button of the current stage
+     */
+    public void setExitButton(JButton exitButton) {
+        this.exitButton = exitButton;
+    }
 
     public abstract GeneratorInfo getGeneratorInfo();
 
