cd81db1e84f71766c0de88e7d1e9c5203433d268
nvcleemp <nico.vancleemput@gmail.com>
2009-03-17 08:33:53 +0000

    reformatted code

--- cage/Utils.java
+++ cage/Utils.java
@@ -1,24 +1,20 @@
-
 package cage;
 
 import java.util.Vector;
 import javax.swing.AbstractButton;
 
+public class Utils {
 
-public class Utils
-{
-  public static void addIfSelected(Vector vector, AbstractButton button, String string)
-  {
-    addIfSelected(vector, button, new String[] { string });
-  }
-  public static void addIfSelected(Vector vector, AbstractButton button, String[] strings)
-  {
-    if (button.isSelected()) {
-      for (int i = 0; i < strings.length; ++i)
-      {
-        vector.addElement(strings[i]);
-      }
+    public static void addIfSelected(Vector vector, AbstractButton button, String string) {
+        addIfSelected(vector, button, new String[]{string});
     }
-  }
+
+    public static void addIfSelected(Vector vector, AbstractButton button, String[] strings) {
+        if (button.isSelected()) {
+            for (int i = 0; i < strings.length; ++i) {
+                vector.addElement(strings[i]);
+            }
+        }
+    }
 }
 
--- cage/ValencyElementRule.java
+++ cage/ValencyElementRule.java
@@ -1,46 +1,41 @@
-
 package cage;
 
 import java.util.Hashtable;
 import lisken.systoolbox.MutableInteger;
 import lisken.systoolbox.Systoolbox;
 
+public class ValencyElementRule implements ElementRule {
 
-public class ValencyElementRule implements ElementRule
-{
-  Hashtable elements = new Hashtable();
+    Hashtable elements = new Hashtable();
 
-  public ValencyElementRule(String elementRule)
-  {
-    String[] rule = Systoolbox.stringToArray(elementRule);
-    int n = 0;
-    for (int i = 0; i < rule.length; ++i)
-    {
-      String entry = rule[i];
-      try {
-        int p = entry.indexOf(':');
-	int k = Integer.parseInt(entry.substring(0, p));
-	entry = entry.substring(p+1);
-	if (entry.length() == 0) throw new Exception();
-	n = k;
-      } catch (Exception e) {
-        while (elements.containsKey(new MutableInteger(++n)))
-	{
-	}
-      }
-      elements.put(new MutableInteger(n), entry);
+    public ValencyElementRule(String elementRule) {
+        String[] rule = Systoolbox.stringToArray(elementRule);
+        int n = 0;
+        for (int i = 0; i < rule.length; ++i) {
+            String entry = rule[i];
+            try {
+                int p = entry.indexOf(':');
+                int k = Integer.parseInt(entry.substring(0, p));
+                entry = entry.substring(p + 1);
+                if (entry.length() == 0) {
+                    throw new Exception();
+                }
+                n = k;
+            } catch (Exception e) {
+                while (elements.containsKey(new MutableInteger(++n))) {
+                }
+            }
+            elements.put(new MutableInteger(n), entry);
+        }
     }
-  }
 
-  public String getElement(EmbeddableGraph graph, int vertex)
-  {
-    int valency = graph.getValency(vertex);
-    return (String) elements.get(new MutableInteger(valency));
-  }
+    public String getElement(EmbeddableGraph graph, int vertex) {
+        int valency = graph.getValency(vertex);
+        return (String) elements.get(new MutableInteger(valency));
+    }
 
-  static public void main(String[] argv)
-  {
-    System.out.println(new ValencyElementRule(argv[0]).elements);
-  }
+    static public void main(String[] argv) {
+        System.out.println(new ValencyElementRule(argv[0]).elements);
+    }
 }
 
