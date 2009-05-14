9bd21f1a05e54d32bc4c190a2edc648b0d5a6d08
nvcleemp <nico.vancleemput@gmail.com>
2009-05-14 11:46:35 +0000

    PlaneTriangulationsPanel: extend CombinedGeneratorPanel

--- cage/generator/PlaneTriangulationsPanel.java
+++ cage/generator/PlaneTriangulationsPanel.java
@@ -1,31 +1,13 @@
 package cage.generator;
 
-import cage.GeneratorInfo;
-import cage.GeneratorPanel;
+import cage.CombinedGeneratorPanel;
 
-import javax.swing.BorderFactory;
-import javax.swing.JTabbedPane;
-
-public class PlaneTriangulationsPanel extends GeneratorPanel {
+public class PlaneTriangulationsPanel extends CombinedGeneratorPanel {
 
     public PlaneTriangulationsPanel() {
-        pane.addTab("general triangulations", new GeneralTriangulationsPanel(false));
-        pane.addTab("Eulerian triangulations", new EulerianTriangulationsPanel(false));
-        pane.addTab("triangulations with given degrees", new CGFPanel(true));
-        for (int i = 0; i < pane.getTabCount(); ++i) {
-            ((GeneratorPanel) pane.getComponentAt(i)).setBorder(
-                    BorderFactory.createEmptyBorder(20, 20, 20, 20));
-        }
+        addTab("general triangulations", new GeneralTriangulationsPanel(false));
+        addTab("Eulerian triangulations", new EulerianTriangulationsPanel(false));
+        addTab("triangulations with given degrees", new CGFPanel(true));
         add(pane);
     }
-
-    public void showing() {
-    }
-
-    public GeneratorInfo getGeneratorInfo() {
-        GeneratorPanel chosenPanel = (GeneratorPanel) pane.getSelectedComponent();
-        GeneratorInfo info = chosenPanel.getGeneratorInfo();
-        return info;
-    }
-    JTabbedPane pane = new JTabbedPane();
 }
