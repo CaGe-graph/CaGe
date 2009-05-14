80a4cb6b1a935dc2178c82ad1f73b7377e8bbc1d
nvcleemp <nico.vancleemput@gmail.com>
2009-05-14 08:21:40 +0000

    ThreeRegularPlaneGraphsPanel: extend CombinedGeneratorPanel instead of GeneratorPanel

--- cage/generator/ThreeRegularPlaneGraphsPanel.java
+++ cage/generator/ThreeRegularPlaneGraphsPanel.java
@@ -1,34 +1,15 @@
 package cage.generator;
 
-import cage.GeneratorInfo;
-import cage.GeneratorPanel;
+import cage.CombinedGeneratorPanel;
 
-import javax.swing.BorderFactory;
-import javax.swing.JTabbedPane;
-
-public class ThreeRegularPlaneGraphsPanel extends GeneratorPanel {
+public class ThreeRegularPlaneGraphsPanel extends CombinedGeneratorPanel {
 
     private static final boolean debug = false;
-    private JTabbedPane pane = new JTabbedPane();
 
     public ThreeRegularPlaneGraphsPanel() {
-        pane.addTab("general cubic plane graphs", new GeneralTriangulationsPanel(true));
-        pane.addTab("fullerenes", new FullgenPanel());
-        pane.addTab("cubic plane graphs with given faces", new CGFPanel(false));
-        pane.addTab("bipartite cubic plane graphs", new EulerianTriangulationsPanel(true));
-        for (int i = 0; i < pane.getTabCount(); ++i) {
-            ((GeneratorPanel) pane.getComponentAt(i)).setBorder(
-                    BorderFactory.createEmptyBorder(20, 20, 20, 20));
-        }
-        add(pane);
-    }
-
-    public void showing() {
-    }
-
-    public GeneratorInfo getGeneratorInfo() {
-        GeneratorPanel chosenPanel = (GeneratorPanel) pane.getSelectedComponent();
-        GeneratorInfo info = chosenPanel.getGeneratorInfo();
-        return info;
+        addTab("general cubic plane graphs", new GeneralTriangulationsPanel(true));
+        addTab("fullerenes", new FullgenPanel());
+        addTab("cubic plane graphs with given faces", new CGFPanel(false));
+        addTab("bipartite cubic plane graphs", new EulerianTriangulationsPanel(true));
     }
 }
