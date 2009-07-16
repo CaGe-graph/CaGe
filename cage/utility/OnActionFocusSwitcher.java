be2222a80e5021f39e8b1951f91fd5601b595371
nvcleemp <nico.vancleemput@gmail.com>
2009-07-16 13:29:39 +0000

    OnActionFocusSwitcher: Added documentation and refactored code

--- cage/utility/OnActionFocusSwitcher.java
+++ cage/utility/OnActionFocusSwitcher.java
@@ -1,6 +1,5 @@
 package cage.utility;
 
-import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.AbstractButton;
@@ -8,16 +7,28 @@ import javax.swing.JComboBox;
 import javax.swing.JComponent;
 
 /**
- *
+ * Implementation of <code>ActionListener</code> that tries to focus a given
+ * component when fired.
  */
 public class OnActionFocusSwitcher implements ActionListener {
 
     private JComponent component;
 
+    /**
+     * Creates a new <code>OnActionFocusSwitcher</code>.
+     * @param c The component that needs to be focused.
+     */
     public OnActionFocusSwitcher(JComponent c) {
         component = c;
     }
 
+    /**
+     * Creates a new <code>OnActionFocusSwitcher</code> and adds it to the
+     * specified <code>JComboBox</code> or <code>AbstractButton</code>.
+     * @param c The component that needs to be focused.
+     * @param target The <code>JComboBox</code> or <code>AbstractButton</code>
+     *               to which needs to be listened
+     */
     public OnActionFocusSwitcher(JComponent c, JComponent target) {
         this(c);
         if (target instanceof JComboBox) {
@@ -29,12 +40,5 @@ public class OnActionFocusSwitcher implements ActionListener {
 
     public void actionPerformed(ActionEvent e) {
         component.requestFocus();
-        Component source = (Component) e.getSource();
-        if (source instanceof AbstractButton) {
-            if (!((AbstractButton) source).isSelected()) {
-                return;
-            }
-        }
-//    source.transferFocus();
     }
 }
