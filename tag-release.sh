9775c6402a4c09a72a2244c83bb6ca5982abaaa2
nvcleemp <nico.vancleemput@gmail.com>
2011-03-04 16:25:05 +0000

    Added script for tagging releases. This should not be included in (source)  distributions, because it is only of interest for people who can commit to the repository. So it is intentionally that this is not included in the Makefile.

---
+++ tag-release.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+repository=svn+ssh://caagt/public/svn-cage/cage
+
+if [ $# -eq 1 ]
+then
+
+    release=$1
+	
+	svn ls ${repository}/tags/${release} > /dev/null 2> /dev/null
+	if [ $? -eq 0 ]
+	then
+	    echo "That release already exists."
+		exit
+	fi
+	
+    svn copy ${repository}/trunk ${repository}/tags/${release} -m "Tagging version ${release}"
+	
+elif [ $# -eq 2 ]
+then
+
+    revision=$1
+    release=$2
+	
+	svn ls ${repository}/tags/${release} > /dev/null 2> /dev/null
+	if [ $? -eq 0 ]
+	then
+	    echo "That release already exists."
+		exit
+	fi
+
+    svn copy -r $revision ${repository}/trunk ${repository}/tags/${release} -m "Tagging version ${release}"
+	
+fi
