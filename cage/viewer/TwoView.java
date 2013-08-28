7b346ffd66864ebe94406782afcce88b5b87b407
nvcleemp <nico.vancleemput@gmail.com>
2013-08-28 14:21:56 +0200

    Removed unused import

--- cage/viewer/TwoView.java
+++ cage/viewer/TwoView.java
@@ -38,7 +38,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
