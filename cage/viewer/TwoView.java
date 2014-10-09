c993e9aeb820a74586cd257df6e7195b1571cdd8
nvcleemp <nico.vancleemput@gmail.com>
2014-10-09 11:50:27 +0200

    Removed unneeded imports

--- cage/viewer/TwoView.java
+++ cage/viewer/TwoView.java
@@ -40,8 +40,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
